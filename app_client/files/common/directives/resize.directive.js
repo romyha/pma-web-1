56a222315afc84eb606ad5801d790c634bd26c66
Romy Haacke <romy@haacke.tv>
2016-08-19 16:52:59 +0200

    Calculate update-list height

--- app_client/files/common/directives/resize.directive.js
+++ app_client/files/common/directives/resize.directive.js
@@ -7,16 +7,19 @@
             restrict: 'AC',
             link: function (scope, element, attrs) {
                 attrs.$observe('resize', function (value) {
-                    
                     //make sure that page is rendered and changed values are applied before checking them
                     $timeout(function () {
+                        console.log(element);
                         if (element[0].localName == 'img') {
                             var infoHeight = document.getElementById('item-info').offsetHeight + 'px';
                             element[0].style.maxHeight = infoHeight;
                         } else if (element[0].localName == 'ul') {
-                            var navHeight = document.getElementsByTagName('nav')[0].offsetHeight;
-                            var infoHeight = document.getElementById('item-info').offsetHeight;
-                            element[0].style.height = document.getElementsByTagName('body')[0].offsetHeight - (infoHeight + navHeight) + 'px';
+                            var navHeight = $('nav').outerHeight(true);
+                            var infoHeight = $('#item-info').outerHeight(true);
+                            var body = $('body').outerHeight(true);
+                            var panel = $('.panel-heading').outerHeight(true);
+                            element[0].style.height = body - infoHeight - navHeight - panel - 25 + 'px';
+                            console.log(body, infoHeight, navHeight, panel, element[0].style.height);
                         }
                     }, 0);
                 });
