7c3dea3f117991e690989a2d475fa3d231148372
Romy Haacke <romy@haacke.tv>
2016-08-19 12:14:06 +0200

    Add validation for add update form

--- app_client/files/addPages/addUpdate.controller.js
+++ app_client/files/addPages/addUpdate.controller.js
@@ -6,6 +6,9 @@
         var vm = this;
         vm.device = device;
         vm.locations = locations;
+        vm.colorValid = true;
+        vm.messageValid = true;
+        vm.locationValid = true;
 
         vm.dismiss = function () {
             $uibModalInstance.close();
@@ -14,6 +17,7 @@
         vm.setStatus = function (color, status) {
             vm.status = status;
             vm.color = color;
+            vm.validate();
         };
 
         vm.dateOptions = {
@@ -24,26 +28,59 @@
 
         vm.update.date = new Date();
 
+        vm.validate = function () {
+            var valid;
+            if (vm.color && !vm.update.message) {
+                vm.error = "Location or message and status required.";
+                vm.messageValid = false;
+                vm.colorValid = true;
+                vm.locationValid = true;
+                valid = false;
+            } else if (vm.update.message && !vm.color) {
+                vm.error = "Location or message and status required.";
+                vm.colorValid = false;
+                vm.messageValid = true;
+                vm.locationValid = true;
+                valid = false;
+            }
+            else if (!vm.update.location && !vm.update.message && !vm.color) {
+                vm.error = "Location or message and status required.";
+                vm.colorValid = false;
+                vm.messageValid = false;
+                vm.locationValid = false;
+                valid = false;
+            } else {
+                vm.error = "";
+                vm.colorValid = true;
+                vm.messageValid = true;
+                vm.locationValid = true;
+                valid = true;
+            }
+            return valid;
+        }
+
         vm.addUpdate = function () {
-            deviceData.addUpdateById(vm.device._id, {
-                author: author,
-                location: vm.update.location,
-                date: vm.update.date,
-                status: vm.color,
-                message: vm.update.message
-            }).success(function (data) {
-                if (!(vm.locations.indexOf(vm.update.location) > -1)) {
-                    deviceData.addLocation({
-                        name: vm.update.location
-                    });
-                }
-                doRefresh();
-                vm.dismiss();
-            }).error(function (err) {
-                if(typeof err == "string" && err.indexOf('Unauthorized') > -1) {
-                    vm.unauthorized = true;
-                }
-            });
+            if (vm.validate()) {
+                deviceData.addUpdateById(vm.device._id, {
+                    author: author,
+                    location: vm.update.location,
+                    date: vm.update.date,
+                    status: vm.color,
+                    message: vm.update.message
+                }).success(function (data) {
+                    if (!(vm.locations.indexOf(vm.update.location) > -1)) {
+                        deviceData.addLocation({
+                            name: vm.update.location
+                        });
+                    }
+                    doRefresh();
+                    vm.dismiss();
+                }).error(function (err) {
+                    if (typeof err == "string" && err.indexOf('Unauthorized') > -1) {
+                        vm.unauthorized = true;
+                    }
+                });
+            }
         };
     }
 })();
