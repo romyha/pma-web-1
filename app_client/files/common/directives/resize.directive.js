c52a1008b39ea7d97d0cf39adb28c22e3c244a7f
Romy Haacke <romy@haacke.tv>
2016-08-22 12:16:58 +0200

    Change height info for item img

--- app_client/files/common/directives/resize.directive.js
+++ app_client/files/common/directives/resize.directive.js
@@ -9,17 +9,17 @@
                 attrs.$observe('resize', function (value) {
                     //make sure that page is rendered and changed values are applied before checking them
                     $timeout(function () {
-                        console.log(element);
                         if (element[0].localName == 'img') {
-                            var infoHeight = document.getElementById('item-info').offsetHeight + 'px';
+                            var infoHeight = document.getElementById('item').offsetHeight + 'px';
                             element[0].style.maxHeight = infoHeight;
+                            console.log(element[0]);
                         } else if (element[0].localName == 'ul') {
                             var navHeight = $('nav').outerHeight(true);
                             var infoHeight = $('#item-info').outerHeight(true);
                             var body = $('body').outerHeight(true);
                             var panel = $('.panel-heading').outerHeight(true);
                             element[0].style.height = body - infoHeight - navHeight - panel - 25 + 'px';
-                            console.log(body, infoHeight, navHeight, panel, element[0].style.height);
+                            
                         }
                     }, 0);
                 });
